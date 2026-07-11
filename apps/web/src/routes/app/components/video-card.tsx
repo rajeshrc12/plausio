501658679b1afd7d3583f15004436dfb10ad72ff
rajeshrc12 <rajesh.charhajari@gmail.com>
2026-07-11 05:37:30 -0400

    web: home page video card added

---
+++ apps/web/src/routes/app/components/video-card.tsx
@@ -0,0 +1,62 @@
+import { Play } from "lucide-react"
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@workspace/ui/components/avatar"
+
+interface Video {
+  id: number
+  title: string
+  duration: string
+  views: string
+  channelName: string
+  createdAt: string
+  thumbnail: string
+}
+
+interface VideoCardProps {
+  video: Video
+}
+
+const VideoCard = ({ video }: VideoCardProps) => {
+  return (
+    <div className="group cursor-pointer">
+      {/* Thumbnail */}
+      <div className="relative overflow-hidden rounded-xl">
+        <img
+          src={video.thumbnail}
+          alt={video.title}
+          className="aspect-video w-full object-cover"
+        />
+
+        <span className="absolute right-2 bottom-2 rounded bg-primary/90 px-1.5 py-0.5 text-xs font-medium text-background">
+          {video.duration}
+        </span>
+      </div>
+
+      {/* Video Details */}
+      <div className="mt-3 flex gap-3">
+        {/* Channel Avatar */}
+
+        <Avatar className={"h-10 w-10"}>
+          <AvatarImage src={`https://i.pravatar.cc/150?img=${video.id}`} />
+          <AvatarFallback>name</AvatarFallback>
+        </Avatar>
+        <div className="flex flex-1 flex-col gap-1">
+          <h3 className="line-clamp-2 leading-5 font-semibold">
+            {video.title}
+          </h3>
+          <div className="flex items-center gap-2 text-sm text-muted-foreground">
+            <div>{video.channelName}</div>
+            <Play size={10} />
+            <div>{video.views}</div>
+            <div>{video.createdAt}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default VideoCard
--- apps/web/src/routes/app/home/index.tsx
+++ apps/web/src/routes/app/home/index.tsx
@@ -1,13 +1,35 @@
 import { Link } from "react-router"
-
+import VideoCard from "@/routes/app/components/video-card"
+const videos = [
+  {
+    id: 1,
+    title: `
+    
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur voluptates tempore quibusdam non nulla eaque, quasi fuga, est ipsam distinctio cumque officia itaque obcaecati doloremque maiores aspernatur autem atque. Nam.
+    
+    How to use React?`,
+    duration: "2:20",
+    views: "300K",
+    channelName: "Rajesh Charhajari",
+    createdAt: "5 hours ago",
+    thumbnail: "https://picsum.photos/seed/picsum/320/180",
+  },
+  {
+    id: 2,
+    title: "React Hooks Explained in 15 Minutes",
+    duration: "15:42",
+    views: "1.2M",
+    channelName: "Code Master",
+    createdAt: "2 days ago",
+    thumbnail: "https://picsum.photos/seed/react/320/180",
+  },
+]
 const Home = () => {
   return (
     <div className="grid grid-cols-3 gap-5 p-5">
-      {new Array(30).fill(0).map((_, i) => (
-        <Link to={`/${i}`}>
-          <div key={i} className="h-50 border">
-            {i}
-          </div>
+      {videos.map((video) => (
+        <Link to={`/${video.id}`}>
+          <VideoCard video={video} />
         </Link>
       ))}
     </div>
