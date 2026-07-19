b85655d052d55853d1ab79e90fc1b7ad3fcebecd
rajeshrc12 <rajesh.charhajari@gmail.com>
2026-07-19 21:19:49 +0530

    client file added

--- apps/upload-service/src/index.ts
+++ apps/upload-service/src/index.ts
@@ -1,10 +1,11 @@
 import "dotenv/config"
 import express from "express"
 import { prisma } from "@workspace/db"
+import { test } from "./app.js"
 const app = express()
 
 const port = Number(process.env.PORT ?? 3000)
-
+test()
 app.get("/", async (_, res) => {
   const users = await prisma.user.findMany()
   console.log(users)
