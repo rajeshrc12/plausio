0b91dcc73f2f2d637914de98cf8eebacb38c2b61
rajeshrc12 <rajesh.charhajari@gmail.com>
2026-07-19 23:44:25 +0530

    prisma added

--- upload-service/src/app.ts
+++ upload-service/src/app.ts
@@ -1,7 +1,13 @@
 import express from "express";
+import { prisma } from "./lib/prisma.js";
 
 const app = express();
 
 app.use(express.json());
+
+app.get("/", async (_, res) => {
+  const user = await prisma.user.findMany();
+  res.status(200).json({ hello: "world", user });
+});
 
 export default app;
