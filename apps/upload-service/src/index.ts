4a470d292e422cf4a190643e1b120bd8e06f6809
rajeshrc12 <rajesh.charhajari@gmail.com>
2026-07-19 17:41:37 +0530

    upload service and prisma added

---
+++ apps/upload-service/src/index.ts
@@ -0,0 +1,20 @@
+import "dotenv/config"
+import express from "express"
+import { prisma } from "@workspace/db"
+const app = express()
+
+const port = Number(process.env.PORT ?? 3000)
+
+app.get("/", async (_, res) => {
+  const users = await prisma.user.findMany()
+  console.log(users)
+  res.json(users)
+})
+
+app.get("/health", (_, res) => {
+  res.send("ok")
+})
+
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Listening on ${port}`)
+})
