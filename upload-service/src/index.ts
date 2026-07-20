39f9c37c38446118e5e1074b176f7356341f812d
rajeshrc12 <rajesh.charhajari@gmail.com>
2026-07-20 20:55:18 +0530

    web app added

--- upload-service/src/index.ts
+++ upload-service/src/index.ts
@@ -2,5 +2,5 @@ import app from "./app.js";
 import { env } from "./config/env.js";
 
 app.listen(env.PORT, () => {
-  console.log(`Upload sevice running on ${env.PORT}`);
+  console.log(`Upload Sevice running on ${env.PORT}`);
 });
