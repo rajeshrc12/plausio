089e57a2783538b20ee9e71151df1211a2421a5e
rajeshrc12 <rajesh.charhajari@gmail.com>
2026-07-20 13:05:37 +0530

    npm upload service added

---
+++ upload-service/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import router from "./routes.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use("/", router);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
