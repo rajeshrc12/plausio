8b57753c10679e49c8092dc08f6ba342e859992c
rajeshrc12 <rajesh.charhajari@gmail.com>
2026-07-20 21:51:49 +0530

    web: libraries added

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,14 +1,19 @@
-import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
 import "./index.css"
-import App from "./App.tsx"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
+import { RouterProvider } from "react-router"
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { Toaster } from "@/components/ui/sonner"
+import router from "@/routes"
+
+const queryClient = new QueryClient()
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </StrictMode>
+  <ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <Toaster />
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </ThemeProvider>
 )
