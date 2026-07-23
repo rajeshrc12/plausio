8e95e85e6c7d94864c27e7cdad6b6a695129622f
rajeshrc12 <rajesh.charhajari@gmail.com>
2026-07-23 12:44:30 +0530

    cleanup

--- upload-service/src/services/google.ts
+++
@@ -1,8 +0,0 @@
-import { OAuth2Client } from "google-auth-library";
-import { env } from "../config/env.js";
-
-export const googleClient = new OAuth2Client(
-  env.GOOGLE_CLIENT_ID,
-  env.GOOGLE_CLIENT_SECRET,
-  env.GOOGLE_FALLBACK_URL,
-);
