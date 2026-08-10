ed05b2f35c1c188b8884181ee3c76bf8b9348a9f
rajeshrc12 <rajesh.charhajari@gmail.com>
2026-08-10 19:58:09 +0530

    transcode service changes

--- transcode-service/src/index.ts
+++ transcode-service/src/index.ts
@@ -1,35 +1,14 @@
-import amqp from "amqplib";
-import { env } from "./config/env.js";
 import { processVideo } from "./services/video.js";
 
 async function main() {
-  const connection = await amqp.connect(env.RABBITMQ_URL);
-  const channel = await connection.createChannel();
+  console.log("Transcode worker starting...");
 
-  await channel.assertQueue(env.RABBITMQ_QUEUE_NAME, {
-    durable: true,
-  });
-
-  await channel.prefetch(1);
-
-  console.log(`Worker listening on "${env.RABBITMQ_QUEUE_NAME}"`);
-
-  await channel.consume(env.RABBITMQ_QUEUE_NAME, async (msg) => {
-    if (!msg) return;
-
-    try {
-      const message = JSON.parse(msg.content.toString());
-
-      await processVideo(message);
-
-      channel.ack(msg);
-    } catch (error) {
-      console.error("Failed to process message:", error);
-
-      // Requeue the message.
-      channel.nack(msg, false, true);
-    }
-  });
+  try {
+    await processVideo({});
+  } catch (error) {
+    console.error("Failed to process message:", error);
+    process.exit(1);
+  }
 }
 
 main().catch((error) => {
--- transcode-service/src/services/video.ts
+++ transcode-service/src/services/video.ts
@@ -1,3 +1,9 @@
 export const processVideo = async (message: any) => {
-  console.log(message);
+  console.log("Transcode worker is running...");
+
+  while (true) {
+    // TODO: Get and process video jobs here
+
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+  }
 };
