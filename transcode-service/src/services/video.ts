e16a9429bf7c489d6be59c7313f9d2a8d61a2ca0
rajeshrc12 <rajesh.charhajari@gmail.com>
2026-08-11 14:33:01 +0530

    transcode service changes

--- transcode-service/src/services/video.ts
+++ transcode-service/src/services/video.ts
@@ -1,22 +1,18 @@
-import axios from "axios";
-import { updateContainerStatus } from "./container.js";
-
 export const processVideo = async (job: any) => {
   console.log("Transcode worker is running...", job.data);
-  const container = await updateContainerStatus({
-    id: "1",
-    status: "busy",
-  });
-  console.log(container);
-  if (process.env.ECS_CONTAINER_METADATA_URI_V4) {
-    console.log(process.env.ECS_CONTAINER_METADATA_URI_V4);
-    try {
-      const response = await axios.get(
-        `${process.env.ECS_CONTAINER_METADATA_URI_V4}/task`,
-      );
-      console.log(response.data);
-    } catch (e) {
-      console.log(e);
-    }
+
+  let id: string | undefined;
+
+  const url = process.env.ECS_CONTAINER_METADATA_URI_V4;
+
+  if (url) {
+    id = url.split("/").pop()?.split("-")[0];
+    console.log("task id", id);
+    // const container = await updateContainerStatus({
+    //   id,
+    //   status: "busy",
+    // });
+
+    // console.log(container);
   }
 };
