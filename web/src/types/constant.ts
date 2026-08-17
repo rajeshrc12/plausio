f82c802a2da211c7cd97ff8de5b76509b19c0e6e
rajeshrc12 <rajesh.charhajari@gmail.com>
2026-08-17 11:37:04 +0530

    web changes

--- web/src/types/constant.ts
+++ web/src/types/constant.ts
@@ -1,5 +1,5 @@
 export const MAX_THUMBNAIL_SIZE = 1 * 1024 * 1024
-export const MAX_VIDEO_SIZE = 50 * 1024 * 1024
+export const MAX_VIDEO_SIZE = 1024 * 1024 * 1024
 
 export const IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png"]
 export const VIDEO_TYPES = ["mp4", "mkv"]
