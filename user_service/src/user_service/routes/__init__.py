f412332b7339e004867368858df5dadcd066f52d
rajeshrc12 <rajesh.charhajari@gmail.com>
2026-09-09 17:54:44 +0530

    cleanup

--- user_service/src/user_service/routes/__init__.py
+++ user_service/src/user_service/routes/__init__.py
@@ -1,7 +1,6 @@
 from user_service.routes.user import router as user_router
 from user_service.routes.auth import router as auth_router
 from user_service.routes.connector import router as connector_router
-from user_service.routes.celery import router as celery_router
 from user_service.routes.chat import router as chat_router
 from user_service.routes.message import router as message_router
 from user_service.routes.chat_connector import router as chat_connector_router
@@ -10,7 +9,6 @@ __all__ = [
     "user_router",
     "auth_router",
     "connector_router",
-    "celery_router",
     "chat_router",
     "message_router",
     "chat_connector_router",
